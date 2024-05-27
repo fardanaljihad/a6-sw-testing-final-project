9315201f6a7a009216c3d01d0456ec6db2b4ddda
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-05-27 14:50:58 +0700

    add login page action

--- src/test/java/com/a6/finalproject/swaglabs/actions/LoginPageActions.java
+++ src/test/java/com/a6/finalproject/swaglabs/actions/LoginPageActions.java
@@ -1,5 +1,37 @@
 package com.a6.finalproject.swaglabs.actions;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import com.a6.finalproject.swaglabs.utils.HelperClass;
+import com.a6.finalproject.swaglabs.locators.LoginPageLocators;
+
+
 public class LoginPageActions {
+  WebDriver driver = HelperClass.getDriver();
+  LoginPageLocators loginLocators;
   
+  public LoginPageActions(WebDriver driver) {
+    this.driver = driver;
+    this.loginLocators = new LoginPageLocators();
+    PageFactory.initElements(driver, loginLocators);
+  }
+
+  // Actions
+  public void enterUsername(String username) {
+    loginLocators.usernameField.sendKeys(username);
+  }
+
+  public void enterPassword(String password) {
+    loginLocators.passwordField.sendKeys(password);
+  }
+
+  public void clickLoginButton() {
+    loginLocators.loginButton.click();
+  }
+
+  public String getMessage() {
+    String message = this.loginLocators.message.getText();
+    return message;
+  }
 }
