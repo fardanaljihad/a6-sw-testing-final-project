0e297823468a03d09b80e945abbaa1af29c227e3
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-05-27 14:51:10 +0700

    add dashboard page locator

---
+++ src/test/java/com/a6/finalproject/swaglabs/locators/DashboardPageLocators.java
@@ -0,0 +1,48 @@
+package com.a6.finalproject.swaglabs.locators;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DashboardPageLocators {
+    @FindBy(xpath = "//*[@id=\"add-to-cart-sauce-labs-backpack\"]")
+    public WebElement addToCartSauceLabsBackpack;
+
+    @FindBy(xpath = "//*[@id=\"add-to-cart-sauce-labs-bike-light\"]")
+    public WebElement addToCartSauceLabsBikeLight;
+
+    @FindBy(xpath = "//*[@id=\"add-to-cart-sauce-labs-bolt-t-shirt\"]")
+    public WebElement addToCartSauceLabsBoltTShirt;
+
+    @FindBy(xpath = "//*[@id=\"add-to-cart-sauce-labs-fleece-jacket\"]")
+    public WebElement addToCartSauceLabsFleeceJacket;
+
+    @FindBy(xpath = "//*[@id=\"add-to-cart-sauce-labs-onesie\"]")
+    public WebElement addToCartSauceLabsOnesie;
+
+    @FindBy(xpath = "//*[@id=\"add-to-cart-test.allthethings()-t-shirt-(red)\"]")
+    public WebElement addToCartTestAllTheThingsTShirtRed;
+
+    @FindBy(xpath = "//*[@id=\"remove-sauce-labs-backpack\"]")
+    public WebElement removeSauceLabsBackpack;
+
+    @FindBy(xpath = "//*[@id=\"remove-sauce-labs-bike-light\"]")
+    public WebElement removeSauceLabsBikeLight;
+
+    @FindBy(xpath = "//*[@id=\"remove-sauce-labs-bolt-t-shirt\"]")
+    public WebElement removeSauceLabsBoltTShirt;
+
+    @FindBy(xpath = "//*[@id=\"remove-sauce-labs-fleece-jacket\"]")
+    public WebElement removeSauceLabsFleeceJacket;
+
+    @FindBy(xpath = "//*[@id=\"remove-sauce-labs-onesie\"]")
+    public WebElement removeSauceLabsOnesie;
+
+    @FindBy(xpath = "//*[@id=\"remove-test.allthethings()-t-shirt-(red)\"]")
+    public WebElement removeTestAllTheThingsTShirtRed;
+
+    @FindBy(xpath = "//*[@id=\"shopping_cart_container\"]/a")
+    public WebElement shoppingCartContainer;
+
+    @FindBy(xpath = "//*[@class=\"inventory_item_name\"]")
+    public WebElement inventoryItemName;
+}
