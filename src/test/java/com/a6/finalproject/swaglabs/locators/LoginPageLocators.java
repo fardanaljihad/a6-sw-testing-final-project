557e8721b8b67d81541d212f5532b9885d446602
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-05-27 14:51:42 +0700

    add login page locator

--- src/test/java/com/a6/finalproject/swaglabs/locators/LoginPageLocators.java
+++ src/test/java/com/a6/finalproject/swaglabs/locators/LoginPageLocators.java
@@ -1,5 +1,19 @@
 package com.a6.finalproject.swaglabs.locators;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPageLocators {
-  
+  @FindBy(xpath = "//*[@id=\"user-name\"]")
+  public WebElement usernameField;
+
+  @FindBy(xpath = "//*[@id=\"password\"]")
+  public WebElement passwordField;
+
+  @FindBy(xpath = "//*[@id=\"login-button\"]")
+  public  WebElement loginButton;
+
+  @FindBy(css = "[data-test='error']")
+  public  WebElement message;
+
 }
