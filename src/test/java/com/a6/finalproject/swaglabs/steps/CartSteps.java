a939bc9531e81d93d1e39e30212c910b3c0930be
fardantif21 <fardan.al.tif421@polban.ac.id>
2024-05-30 10:55:54 +0700

    add steps using the snippets

---
+++ src/test/java/com/a6/finalproject/swaglabs/steps/CartSteps.java
@@ -0,0 +1,52 @@
+package com.a6.finalproject.swaglabs.steps;
+
+import org.openqa.selenium.WebDriver;
+
+import com.a6.finalproject.swaglabs.actions.CartPageActions;
+import com.a6.finalproject.swaglabs.utils.HelperClass;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CartSteps {
+  WebDriver driver = HelperClass.getDriver();
+  CartPageActions cartPageActions = new CartPageActions(driver);
+
+  @Given("User has logged in with valid credentials")
+  public void user_has_logged_in_with_valid_credentials() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Given("User is on the dashboard page")
+  public void user_is_on_the_dashboard_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("User clicks the Add to cart button on the {string} product")
+  public void user_clicks_the_add_to_cart_button_on_the_product(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("User clicks the cart icon")
+  public void user_clicks_the_cart_icon() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("User is navigated to the cart page")
+  public void user_is_navigated_to_the_cart_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+  
+  @Then("User sees the {string} product")
+  public void user_sees_the_product(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+  
+}
