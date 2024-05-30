97068f7761e627e277966a34269ea6d41b0bb69b
fardantif21 <fardan.al.tif421@polban.ac.id>
2024-05-30 12:08:39 +0700

    add product detail steps using snippets

---
+++ src/test/java/com/a6/finalproject/swaglabs/steps/ProductDetailSteps.java
@@ -0,0 +1,64 @@
+package com.a6.finalproject.swaglabs.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ProductDetailSteps {
+
+  // Background
+  @Given("User has logged in with valid credentials")
+  public void user_has_logged_in_with_valid_credentials() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  // Scenario 1
+  @Given("User is on the dashboard page")
+  public void user_is_on_the_dashboard_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @When("User clicks on the product image of {string} on the dashboard page")
+  public void user_clicks_on_the_product_image_of_on_the_dashboard_page(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  @Then("User sees the image, name, description, and price information of {string}")
+  public void user_sees_the_image_name_description_and_price_information_of(String string) {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+  // Scenario 2
+  @Given("User is on the product detail page")
+  public void user_is_on_the_product_detail_page() {
+      // Write code here that turns the phrase above into concrete actions
+      throw new io.cucumber.java.PendingException();
+  }
+  @When("User clicks the add to cart button")
+  public void user_clicks_the_add_to_cart_button() {
+      // Write code here that turns the phrase above into concrete actions
+      throw new io.cucumber.java.PendingException();
+  }
+  @Then("The Product is added to the cart")
+  public void the_product_is_added_to_the_cart() {
+      // Write code here that turns the phrase above into concrete actions
+      throw new io.cucumber.java.PendingException();
+  }
+
+  // Scenario 3
+  @When("User clicks the back to products button")
+  public void user_clicks_the_back_to_products_button() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+  @Then("User is navigated to the dashboard page")
+  public void user_is_navigated_to_the_dashboard_page() {
+    // Write code here that turns the phrase above into concrete actions
+    throw new io.cucumber.java.PendingException();
+  }
+
+}
--- src/test/resources/com/a6/finalproject/swaglabs/features/product_detail.feature
+++ src/test/resources/com/a6/finalproject/swaglabs/features/product_detail.feature
@@ -1,10 +1,10 @@
 Feature: Product Detail
 
   Background:
-    # Given User has logged in with valid credentials
+    Given User has logged in with valid credentials
   
   Scenario: User views product details
-    # Given User is on the dashboard page
+    Given User is on the dashboard page
     When User clicks on the product image of "<item_name>" on the dashboard page
     Then User sees the image, name, description, and price information of "<item_name>"
     
@@ -13,12 +13,12 @@ Feature: Product Detail
       | Sauce Labs Backpack      |
       | Sauce Labs Fleece Jacket |
 
+  Scenario: User adds product to cart
+    Given User is on the product detail page
+    When User clicks the add to cart button
+    Then The product is added to the cart
+
   Scenario: User back to the dashboard page from the product detail page
     Given User is on the product detail page
     When User clicks the back to products button
     Then User is navigated to the dashboard page
-
-  Scenario: User adds product to cart
-    Given User is on the product detail page
-    When User clicks the add to cart button
-    Then The Product is added to the cart
