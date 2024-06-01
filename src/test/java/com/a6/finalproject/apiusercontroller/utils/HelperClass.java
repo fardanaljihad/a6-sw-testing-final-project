8a6fa0495fdecb4663c76cde58ad52f26dc0b55a
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-06-01 13:17:41 +0700

    add helper class for api testing

--- src/test/java/com/a6/finalproject/apiusercontroller/utils/HelperClass.java
+++ src/test/java/com/a6/finalproject/apiusercontroller/utils/HelperClass.java
@@ -1,5 +1,12 @@
 package com.a6.finalproject.apiusercontroller.utils;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
 public class HelperClass {
-  
+
+    public static String loadJsonFromFile(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
 }
