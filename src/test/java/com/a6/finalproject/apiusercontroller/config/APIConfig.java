6bf160a4cd6cd609393845f286a8b22fa377e8d3
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-06-01 13:17:22 +0700

    add config for api testing

--- src/test/java/com/a6/finalproject/apiusercontroller/config/APIConfig.java
+++ src/test/java/com/a6/finalproject/apiusercontroller/config/APIConfig.java
@@ -1,5 +1,23 @@
 package com.a6.finalproject.apiusercontroller.config;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.http.ContentType;
+
 public class APIConfig {
-  
+    private static final String BASE_URI = "https://dummyapi.io/data/v1/";
+    private static final String API_KEY = "6637405060f3319dc7a47ca2";
+
+    static {
+        // Setting the baseURI globally for RestAssured
+        RestAssured.baseURI = BASE_URI;
+    }
+
+    public static RequestSpecification getDefaultRequestSpecification() {
+        return new RequestSpecBuilder()
+                .addHeader("app-id", API_KEY)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
 }
